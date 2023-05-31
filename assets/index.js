830cd51f85dc111230ae883ea8541fdd974a2dad
Daniel Cox <daniel.cox2685@gmail.com>
2023-05-31 14:17:55 -0500

    added firebase dependancies so a forgot password tool can be created

--- assets/index.js
+++ assets/index.js
@@ -1,24 +1,32 @@
 const loginForm = document.querySelector('#loginForm');
 const API = 'https://646525da228bd07b3543e0a9.mockapi.io/userLogin'
 
-loginForm.addEventListener('submit', async ({ target: { elements: { username, password, rememberMe } }, preventDefault }) => {
-  preventDefault();
+loginForm.addEventListener('submit', async (event) => {
+  event.preventDefault();
 
-  const response = await fetch(API, {
-    method: 'POST',
-    body: JSON.stringify({ username: username.value, password: password.value }),
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  });
+  const { username, password, rememberMe } = event.target.elements;
+  const userData = { username: username.value, password: password.value };
+  
+  try {
+    const response = await fetch(API, {
+      method: 'POST',
+      body: JSON.stringify(userData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
 
-  if (response.ok) {
-    if (rememberMe.checked) {
-      localStorage.setItem('username', username.value);
-      localStorage.setItem('password', password.value);
+    if (response.ok) {
+      if (rememberMe.checked) {
+        localStorage.setItem('username', username.value);
+        localStorage.setItem('password', password.value);
+      }
+      window.location.href = '/dashboard';
+    } else {
+      alert('Invalid username or password');
     }
-    window.location.href = '/dashboard';
-  } else {
-    alert('Invalid username or password');
+  } catch (error) {
+    console.error('Error:', error);
+    alert('Something went wrong, please try again later');
   }
 });
